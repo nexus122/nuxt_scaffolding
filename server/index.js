899b2ec4eea7260ba003d29f8be2ea71d104d9a0
nexus122 <juan-pablo122@hotmail.es>
2023-09-30 22:45:15 +0200

    Atributo tittle y meta description ( wip) para conseguir un 100 de google ligthhouse

--- server/index.js
+++ server/index.js
@@ -2,28 +2,28 @@
 const pageData = require("../page_data.json");
 const fs = require("fs");
 
+/* --- Funcionalidades --- */
 // ELiminador de acentos
 const removeAccents = (str) => {
-  console.log(str)
   return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-} 
+}
 
 // iterar sobre el array de objetos y crear un archivo dentro de la carpeta pages por cada objeto
 let folderName = pageData[0]["name"];
 let questions = pageData[0]["questions"];
+let metaDescription = pageData[0]["description"];
 
 // Quitamos los acentos a los titulos
-questions.map((element)=>{
+questions.map((element) => {
   element.question = removeAccents(element.question);
 })
 
-// Eliminamos todo lo que haya en la carpeta
-deleteFolder(`../src/pages/${folderName}`);
-
 // Añadimos la estructura al index.vue de la carpeta /nombreProyecto
 fs.writeFileSync(
-  `../src/pages/${folderName}/index.vue`,
+  `../src/pages/index.vue`,
   `<template>
+  <title>${folderName}</title>
+  <meta name="${metaDescription}" />
     <div class="container">
       <article>
         <header>
@@ -36,8 +36,9 @@ fs.writeFileSync(
 // Creamos cada una de las paginas del proyecto
 questions.forEach((page) => {
   fs.writeFileSync(
-    `../src/pages/${folderName}/${page.question.replaceAll(" ", "_")}.vue`,
+    `../src/pages/${page.question.replaceAll(" ", "_")}.vue`,
     `<template>
+        <title>${page.question}</title>
         <div class="container">
           <article>
               <header>
@@ -45,18 +46,37 @@ questions.forEach((page) => {
               </header>
               <p>${page.text}</p>
               <footer>
-                <router-link to="/${folderName}"><button>Back</button></router-link>
+                <router-link to="/"><button>Back</button></router-link>
               </footer>
           </article>
         </div>
     </template>
+    <script>
+    export default {
+      // Otras configuraciones de la página
+    
+      head() {
+        return {
+          title: '${page.question}',
+          meta: [
+            {
+              hid: 'description',
+              name: 'description',
+              content: '${page.text.slice(0, 101)}',
+            },
+          ],
+        }
+      },
+    }
+    </script>    
     `
   );
   // write link in the index.vue file
   fs.appendFileSync(
-    `../src/pages/${folderName}/index.vue`,
-    `<p>
-    <router-link style="text-transform: capitalize;" to="/${folderName}/${page.question
+    `../src/pages/index.vue`,
+    `
+    <p>
+    <router-link style="text-transform: capitalize;" to="/${page.question
       .replaceAll(" ", "_")
       .toUpperCase()}">${page.question}</router-link>
     </p>`
@@ -65,31 +85,28 @@ questions.forEach((page) => {
 
 // Cerramos el archivo index.vue
 fs.appendFileSync(
-  `../src/pages/${folderName}/index.vue`,
+  `../src/pages/index.vue`,
   `</article>  
 </div>
-    </template>`
-);
+    </template>
+    <script>
+export default {
+  // Otras configuraciones de la página
 
-function deleteFolder(carpetaAEliminar){
-fs.readdir(carpetaAEliminar, (err, files) => {
-  if (err) {
-    console.error(`Error al leer la carpeta: ${err}`);
-    return;
-  }
-
-  // Iterar sobre los archivos y eliminar cada uno de ellos
-  files.forEach((file) => {
-    const filePath = path.join(carpetaAEliminar, file);
-
-    fs.unlink(filePath, (err) => {
-      if (err) {
-        console.error(`Error al eliminar ${filePath}: ${err}`);
-      } else {
-        console.log(`Se eliminó ${filePath}`);
-      }
-    });
-  });
-});
-
+  head() {
+    return {
+      title: '${folderName}',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: '${metaDescription}',
+        },
+      ],
+    }
+  },
 }
+</script>
+
+    `
+);
